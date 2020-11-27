3628edc6e5946ee75d54a5f855fab2647b6f895c
Kuasa5Indra <adityaindra135@gmail.com>
2020-11-27 12:54:26 +0700

    Login Feature

---
+++ Login/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Velacro.Api;
+using Velacro.Basic;
+
+namespace CLARA_Desktop.Login
+{
+    class LoginController : MyController
+    {
+        public LoginController(IMyView _myView) : base(_myView)
+        {
+
+        }
+
+        public async void Login(String email, String password)
+        {
+            var client = new ApiClient("http://localhost:8000/");
+            var request = new ApiRequestBuilder();
+
+            var req = request
+                .buildHttpRequest()
+                .addParameters("email", email)
+                .addParameters("password", password)
+                .setEndpoint("login")
+                .setRequestMethod(HttpMethod.Post);
+            var response = await client.sendRequest(request.getApiRequestBundle());
+            Console.WriteLine(response.getJObject()["token"]);
+            client.setAuthorizationToken(response.getJObject()["token"].ToString());
+        }
+    }
+}
