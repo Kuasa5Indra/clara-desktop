44127aa4857878b6d2aa785dd50abda12fe21fbd
Kuasa5Indra <adityaindra135@gmail.com>
2020-11-27 20:16:27 +0700

    Dashboard Features

---
+++ Dashboard/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Velacro.Api;
+using Velacro.Basic;
+
+namespace CLARA_Desktop.Dashboard
+{
+    class DashboardController : MyController
+    {
+        public DashboardController(IMyView _myView) : base(_myView)
+        {
+        }
+    }
+}
